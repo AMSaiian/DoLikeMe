06b6bc6ce36b9777ac2b6b0ef095aee669c302cc
AMSaiian <artursaiyan56@gmail.com>
2024-09-09 17:04:42 +0300

    Enhanced project skeleton with shared assemblies

---
+++ backend/AMSaiian.Shared.Web/Filters/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,133 @@
+using AMSaiian.Shared.Application.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using UnauthorizedAccessException = AMSaiian.Shared.Application.Exceptions.UnauthorizedAccessException;
+
+namespace AMSaiian.Shared.Web.Filters;
+
+public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+{
+    private readonly IReadOnlyDictionary<Type, Action<ExceptionContext>> _exceptionHandlers;
+
+    public ApiExceptionFilterAttribute()
+    {
+        _exceptionHandlers = new Dictionary<Type, Action<ExceptionContext>>
+            {
+                { typeof(ValidationException), HandleValidationException },
+                { typeof(NotFoundException), HandleNotFoundException },
+                { typeof(UnauthorizedAccessException), HandleUnauthorizedAccessException },
+                { typeof(ForbiddenAccessException), HandleForbiddenAccessException },
+                { typeof(ConflictException), HandleConflictException },
+            };
+    }
+
+
+    public override void OnException(ExceptionContext context)
+    {
+        HandleException(context);
+
+        base.OnException(context);
+    }
+
+    private void HandleException(ExceptionContext context)
+    {
+        Type type = context.Exception.GetType();
+
+        if (_exceptionHandlers.TryGetValue(type, out Action<ExceptionContext>? handler))
+        {
+            handler(context);
+        }
+    }
+
+    private void HandleValidationException(ExceptionContext context)
+    {
+        var exception = (ValidationException)context.Exception;
+
+        var details = new ValidationProblemDetails(exception.Errors)
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+        };
+
+        context.Result = new BadRequestObjectResult(details);
+
+        context.ExceptionHandled = true;
+    }
+
+    private void HandleNotFoundException(ExceptionContext context)
+    {
+        var exception = (NotFoundException)context.Exception;
+
+        var details = new ProblemDetails
+        {
+            Status = StatusCodes.Status404NotFound,
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            Title = "The specified resource was not found.",
+            Detail = exception.Message
+        };
+
+        context.Result = new NotFoundObjectResult(details);
+
+        context.ExceptionHandled = true;
+    }
+
+    private void HandleUnauthorizedAccessException(ExceptionContext context)
+    {
+        var exception = (UnauthorizedAccessException)context.Exception;
+
+        var details = new ProblemDetails
+        {
+            Status = StatusCodes.Status401Unauthorized,
+            Title = "Unauthorized",
+            Type = "https://tools.ietf.org/html/rfc7235#section-3.1",
+            Detail = exception.Message
+        };
+
+        context.Result = new ObjectResult(details)
+        {
+            StatusCode = StatusCodes.Status401Unauthorized
+        };
+
+        context.ExceptionHandled = true;
+    }
+
+    private void HandleForbiddenAccessException(ExceptionContext context)
+    {
+        var exception = (ForbiddenAccessException)context.Exception;
+
+        var details = new ProblemDetails
+        {
+            Status = StatusCodes.Status403Forbidden,
+            Title = "Forbidden",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.3",
+            Detail = exception.Message
+        };
+
+        context.Result = new ObjectResult(details)
+        {
+            StatusCode = StatusCodes.Status403Forbidden
+        };
+
+        context.ExceptionHandled = true;
+    }
+
+    private void HandleConflictException(ExceptionContext context)
+    {
+        var exception = (ConflictException)context.Exception;
+
+        var details = new ProblemDetails
+        {
+            Status = StatusCodes.Status409Conflict,
+            Title = "Conflict occured during processing request",
+            Detail = exception.Message,
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.8"
+        };
+
+        context.Result = new ObjectResult(details)
+        {
+            StatusCode = StatusCodes.Status409Conflict
+        };
+
+        context.ExceptionHandled = true;
+    }
+}
--- backend/src/Task.io/ConfigureServices.cs
+++ backend/src/Task.io/ConfigureServices.cs
@@ -1,6 +1,38 @@
-﻿namespace Task.io;
+﻿using System.Reflection;
+using AMSaiian.Shared.Web.Filters;
+using AMSaiian.Shared.Web.Middlewares;
+
+namespace Task.io;
 
 public static class ConfigureServices
 {
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
 
+        services.AddProblemDetails();
+
+        services.Configure<RouteOptions>(options =>
+        {
+            options.LowercaseUrls = true;
+            options.LowercaseQueryStrings = true;
+        });
+
+        services.AddControllers(opts => opts.Filters.Add<ApiExceptionFilterAttribute>());
+
+        services
+            .AddExceptionHandler<GlobalExceptionHandler>()
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen()
+            .AddAutoMapper(configuration =>
+                               configuration.AddMaps(Assembly.GetExecutingAssembly()))
+            .AddCustomServices();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCustomServices(this IServiceCollection services)
+    {
+        return services;
+    }
 }
