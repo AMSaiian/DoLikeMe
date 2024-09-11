da0f4f3075fc22f9aa2fb1e9876431d2d777e42f
AMSaiian <artursaiyan56@gmail.com>
2024-09-11 15:44:59 +0300

    Added new abstractions + package managing

---
+++ backend/src/Shared/AMSaiian.Shared.Domain/Interfaces/IFiltered.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AMSaiian.Shared.Domain.Interfaces;
+
+public interface IFiltered
+{
+    public static abstract ReadOnlyDictionary<string, dynamic> FilteredBy { get; }
+}
---
+++ backend/src/Shared/AMSaiian.Shared.Domain/Interfaces/IRanged.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AMSaiian.Shared.Domain.Interfaces;
+
+public interface IRanged
+{
+    public static abstract ReadOnlyDictionary<string, dynamic> RangedBy { get; }
+}
